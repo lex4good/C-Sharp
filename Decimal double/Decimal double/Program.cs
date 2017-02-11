02087bdc7793951a9ed3207c9519dd297b6a7111
lex4good <lex4good@gmail.com>
2017-02-11 02:07:18 +0200

    First projects
    First projects

---
+++ Decimal double/Decimal double/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal e = 2.4357839456783476m;
+            double pi = 3.14893473927539234;
+
+            Console.Write("pi равно :         ");
+            Console.WriteLine(pi);
+            Console.Write(" Число Єйлера e равно :         ");
+            Console.WriteLine(e);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Formatting/Formatting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "\nмоя строка 1";
+            string b = "\tмоя строка 1";
+            string c = "\aмоя строка 1";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
