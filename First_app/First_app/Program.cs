615e27cea3c5fe75e2d49c326a96290f946a113d
lex4good <lex4good@gmail.com>
2017-06-09 15:41:33 +0300

    09062017

---
+++ First_app/First_app/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace First_app
+{
+	class Program
+				
+	{
+		public static int incrementation (ref int num)
+			{		
+			
+			return num++;
+		
+		}			
+		
+		public static void Main(string[] args)
+		{
+			int a = Convert.ToInt32 (Console.ReadLine()) ;
+			//++a;
+			incrementation (ref a);
+			Console.WriteLine (a);
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+			}
+		
+		
+	}
+}
